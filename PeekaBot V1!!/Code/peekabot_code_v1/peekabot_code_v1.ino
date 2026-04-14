d8c6c39636d8eec6a82f4e2eb01e7a705796aa14
Parth Dhiman <parthdhiman.pd@gmail.com>
2026-04-14 13:08:29 +0530

    UPDATED AND IMPROVED CODE

--- PeekaBot V1!!/Code/peekabot_code_v1/peekabot_code_v1.ino
+++ PeekaBot V1!!/Code/peekabot_code_v1/peekabot_code_v1.ino
@@ -22,7 +22,7 @@
 #define VSYNC_GPIO_NUM    25
 #define HREF_GPIO_NUM     23
 #define PCLK_GPIO_NUM     22
-#define LED_GPIO_NUM   4
+#define LED_GPIO_NUM      4
 
 const char* ssid = "Redmi_Note_10T_5G";
 const char* password = "88888888";
@@ -41,6 +41,7 @@ byte *getNextBuffer();
 int getNextBufferLen();
 
 bool dataAvailable = false;
+bool autoflash = true;
 
 void setflash(bool state){
   digitalWrite(LED_GPIO_NUM, state ? HIGH : LOW);
@@ -54,107 +55,119 @@ void setquality(int quality){
 }
 
 void handleNewMessages(int numNewMessages){
-  Serial.println("handleNewMessages");
-  Serial.println(String(numNewMessages));
-
   for (int i = 0; i < numNewMessages; i++){
+
     String chat_id = String(bot.messages[i].chat_id);
     String text = bot.messages[i].text;
 
-    String from_name = bot.messages[i].from_name;
-    if(from_name == "")
-      from_name = "Guest";
+    if(text == "/photo"){
 
-    if(text == "/photo")
-    {
-      fb = NULL;
+      if(autoflash){
+        setflash(true);
+        delay(200);
+      }
+
+      // Flush both buffered frames (fb_count=2)
+      for(int j = 0; j < 2; j++){
+        camera_fb_t *discard = esp_camera_fb_get();
+        if(discard){
+          esp_camera_fb_return(discard);
+        }
+      }
+
+      // Wait for fresh frame to be captured
+      delay(100);
+
+      // Get the fresh frame
       fb = esp_camera_fb_get();
+
+      if(autoflash){
+        setflash(false);
+      }
+
       if(!fb){
-        Serial.println("camera capture failed");
-        bot.sendMessage(chat_id, "camera capture failed", "");
+        bot.sendMessage(chat_id, "Camera capture failed", "");
         return;
       }
+
       dataAvailable = true;
-      Serial.println("Sending");
+
       bot.sendPhotoByBinary(chat_id, "image/jpeg", fb->len,
                             isMoreDataAvailable, nullptr,
                             getNextBuffer, getNextBufferLen);
 
-      Serial.println("done!");
-
       esp_camera_fb_return(fb);
     }
-    
+
     if (text == "/start"){
-      String welcome = "Welcome to the Peekabot Telegram bot.\n\n";
-      welcome += "/photo : will take a photo\n";
+      String welcome = "Welcome to *Peekabot*\n\n";
+      welcome += "Use /help to see commands";
       bot.sendMessage(chat_id, welcome, "Markdown");
     }
-    if(text=="/flash on"){
+
+    if(text == "/flash on"){
       setflash(true);
-      bot.sendMessage(chat_id, "Flash ON ","");
+      bot.sendMessage(chat_id, "Flash ON", "");
     }
+
     if(text == "/flash off"){
       setflash(false);
-      bot.sendMessage(chat_id,"Flash OFF", "");
+      bot.sendMessage(chat_id, "Flash OFF", "");
     }
-    if(text.startsWith("/quality")) {
-  int q = text.substring(9).toInt();
 
-  if(q >= 10 && q <= 63) {
-    setquality(q);
-    bot.sendMessage(chat_id, "Quality set to " + String(q), "");
-  } else {
-    bot.sendMessage(chat_id, "Enter value between 10-63", "");
-  }
-}
-  }
-}
+    if(text.startsWith("/quality")){
+      int q = text.substring(9).toInt();
 
-bool isMoreDataAvailable()
-{
-  if (dataAvailable)
-  {
-    dataAvailable = false;
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
+      if(q >= 10 && q <= 63){
+        setquality(q);
+        bot.sendMessage(chat_id, "Quality set to " + String(q), "");
+      } else {
+        bot.sendMessage(chat_id, "Enter value between 10-63", "");
+      }
+    }
 
-byte *getNextBuffer()
-{
-  if (fb)
-  {
-    return fb->buf;
-  }
-  else
-  {
-    return nullptr;
+    if(text == "/autoflash on"){
+      autoflash = true;
+      bot.sendMessage(chat_id, "Auto Flash ON", "");
+    }
+
+    if(text == "/autoflash off"){
+      autoflash = false;
+      bot.sendMessage(chat_id, "Auto Flash OFF", "");
+    }
+
+    if(text == "/help"){
+      String help = "*Peekabot Commands*\n\n";
+      help += "/start - Start bot\n";
+      help += "/photo - Take photo\n";
+      help += "/flash on/off - Control flash\n";
+      help += "/autoflash on/off - Auto flash control\n";
+      help += "/quality X - Set quality (10-63)\n";
+
+      bot.sendMessage(chat_id, help, "Markdown");
+    }
   }
 }
 
-int getNextBufferLen()
-{
-  if (fb)
-  {
-    return fb->len;
-  }
-  else
-  {
-    return 0;
-  }
+bool isMoreDataAvailable(){
+  return dataAvailable;
 }
 
-void setup() {
+byte *getNextBuffer(){
+  dataAvailable = false;
+  return fb ? fb->buf : nullptr;
+}
+
+int getNextBufferLen(){
+  return fb ? fb->len : 0;
+}
+
+void setup(){
   Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
+
   pinMode(LED_GPIO_NUM, OUTPUT);
   digitalWrite(LED_GPIO_NUM, LOW);
-  
+
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -175,71 +188,33 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
   config.pixel_format = PIXFORMAT_JPEG;
-  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  config.fb_location = CAMERA_FB_IN_PSRAM;
-  config.jpeg_quality = 12;
-  config.fb_count = 1;
-
-  if (config.pixel_format == PIXFORMAT_JPEG) {
-    if (psramFound()) {
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    config.frame_size = FRAMESIZE_240X240;
-  }
-
-#if CONFIG_IDF_TARGET_ESP32S3
+  config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 10;
   config.fb_count = 2;
-#endif
 
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-  
-  Serial.print("Connecting to Wifi SSID ");
-  Serial.print(ssid);
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+
+  esp_camera_init(&config);
+
   WiFi.begin(ssid, password);
   secured_client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
-  
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
+
+  while (WiFi.status() != WL_CONNECTED){
     delay(500);
   }
-  Serial.print("\nWiFi connected. IP address: ");
-  Serial.println(WiFi.localIP());
 
-  Serial.print("Retrieving time: ");
   configTime(0, 0, "pool.ntp.org");
-  time_t now = time(nullptr);
-  while (now < 24 * 3600)
-  {
-    Serial.print(".");
-    delay(100);
-    now = time(nullptr);
-  }
-  Serial.println(now);
 
   bot.longPoll = 60;
 }
 
-void loop() {
-  if (millis() - bot_lasttime > BOT_MTBS)
-  {
+void loop(){
+
+  if (millis() - bot_lasttime > BOT_MTBS){
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
-    while (numNewMessages)
-    {
-      Serial.println("got response");
+    while (numNewMessages){
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
